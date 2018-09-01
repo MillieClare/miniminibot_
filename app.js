a9ce03cbb8ca48e89cecee14f1da9250ce2956c7
MillieClare <40922831+MillieClare@users.noreply.github.com>
2018-09-01 16:54:06 +0100

    Added !uptime and !hi

--- app.js
+++ app.js
@@ -12,17 +12,18 @@ const options = {
     },
     identity: {
         username: "miniminibot_",
-        password: ""
+        password: "oauth:0l1ddwius541pn3vjcorz2rwiwdk0s"
     },
     channels: ["Milliebug_"]
 
 };
 
-let knownCommands = { twitter, commands, so, time };
+let knownCommands = { twitter, commands, so, time, sub, follow, hype, lurk, discord, hi, uptime };
 let commandPrefix = '!';
 
 const client = new tmi.client(options);
 client.connect();
+
 
 
 function twitter(target, context, params) {
@@ -30,11 +31,11 @@ function twitter(target, context, params) {
 }
 
 function commands(target, context, params) {
-    client.say("Milliebug_", "Current commands can be found here: ");
+    client.say("Milliebug_", "Current commands can be found here: https://bit.ly/2BZSGAM");
 }
 
 function so(target, context, params) {
-    client.say("Milliebug_", `Thank you ${params[0]} for supporting the channel - make sure to show them some love!`);
+    client.say("Milliebug_", `Thank you twitch.tv/${params[0]} for supporting the channel - make sure to show them some love!`);
 }
 
 function time(target, context, params) {
@@ -42,6 +43,67 @@ function time(target, context, params) {
     client.say("Milliebug_", `Millie's timezone is BST, the current time is ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`);
 
 }
+
+function hi(target, context, params) {
+    let responses = [`Heya ${context.username}, how are you today?`, `Hi ${context.username}, how has your day been?`, `Yo yo yo ${context.username}, how's it hangin'?`];
+    
+    let responseNumber = Math.floor(Math.random() * responses.length);
+    client.say("Milliebug_", responses[responseNumber]);
+}
+
+function sub(target, context, params) {
+    client.say("Milliebug_", "Enjoying the stream? Want your own song? Click here -> twitch.tv/products/milliebug_ or use Twitch Prime to sub for free!");
+}
+
+function follow(target, context, params) {
+    client.say("Milliebug_", "Smash that follow button for a cookie <3");
+}
+
+function hype(target, context, params) {
+    client.say("Milliebug_", "TwitchUnity millie4Minihype MorphinTime millie4Minihype KAPOW millie4Minihype MorphinTime millie4Minihype TwitchUnity millie4Minihype TwitchUnity millie4Minihype MorphinTime millie4Minihype KAPOW millie4Minihype MorphinTime millie4Minihype TwitchUnity");
+}
+
+function lurk(target, context, params) {
+    client.say("Milliebug_", "Lurk mode activated! Remember that when you mute a stream you do not count as a viewer! Please mute the tab or window instead - you rock!");
+}
+
+function discord(target, context, params) {
+    client.say("Milliebug_", "Wanna join my Discord family? Click here: https://discord.gg/E2zvvhn");
+}
+
+
+
+function uptime(target, context, params) {
+    let currentTime = new Date();
+    let timeDifference = currentTime.getTime() - startTime.getTime();
+    console.log(timeDifference);
+
+    let hours = Math.floor(timeDifference / (1000 * 60 * 60));
+    let minutes = Math.floor(timeDifference / (1000 * 60)) % 60;
+    let seconds = Math.floor(timeDifference / 1000) % 60;
+
+    let hoursWord = 'hours';
+    let minutesWord = 'minutes';
+    let secondsWord = 'seconds';
+
+    if (seconds === 1) {
+        secondsWord = 'second';
+    }
+
+    if (minutes === 1) {
+        minutesWord = 'minute';
+    }
+
+    if (hours === 1) {
+        hoursWord = 'hour';
+    }
+
+    client.say("Milliebug_", `Millie has been live for ${hours} ${hoursWord}, ${minutes} ${minutesWord} and ${seconds} ${secondsWord}. `);
+
+
+}
+
+
 
 function onMessageHandler(target, context, msg, self) {
     if (self) {
@@ -73,8 +135,16 @@ function onMessageHandler(target, context, msg, self) {
 
 client.on("message", onMessageHandler);
 
+
+let startTime;
+
+
 client.on("connected", function (address, port) {
     console.log("Address: " + address + " Port: " + port);
+    startTime = new Date();
+    console.log(startTime);
+
 });
+
 
 
